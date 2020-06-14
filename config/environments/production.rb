7fa30999fa84662f0ecf06011ef1fd9e5938e86f
itachi <neeraj.gupta@oyorooms.com>
2020-06-15 01:41:07 +0530

    blue minimalistic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.quiet = true
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
